c69fa92a9eecb60ca72eb0aca751761ec6990258
Alan Kuo <161139744+brotatokuo@users.noreply.github.com>
2025-06-07 22:46:49 -0500

    remove sudo

--- install_docker.sh
+++ install_docker.sh
@@ -1,21 +1,22 @@
 # Update package index
-sudo apt update
+apt update
 
 # Install prerequisites
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 # Add Docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Add Docker repository
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
 
 # Install Docker
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io
 
 # Install Docker Compose
-sudo apt install docker-compose-plugin
+apt install -y docker-compose-plugin
 
-sudo systemctl start docker
-sudo systemctl enable docker
+# Start and enable Docker service
+systemctl start docker
+systemctl enable docker
